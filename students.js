7e45b9955142ceea951a9816dc700e2ae85a1fa4
배주영 <baejuyoung@baejuyeong-ui-MacBookAir.local>
2024-07-17 15:45:30 +0900

    2번 문제(filter)

--- students.js
+++ students.js
@@ -7,7 +7,7 @@ const students = [
     {name: 'Eve', scores: {math: 90, english: 93, science: 99}}
 ];
 
-// 학생들의 평균 점수 계산(map)
+// 1. 학생들의 평균 점수 계산(map)
 const studentsWithAverage = students.map(function (student) {
     let studentScore = Object.values(student.scores);
     let studentAvg = studentScore.reduce(function (acc, cur) {
@@ -18,32 +18,34 @@ const studentsWithAverage = students.map(function (student) {
 console.log("학생들의 평균 점수:", studentsWithAverage);
 
 
-// 평균 점수가 85점 이상인 학생 필터링(filter)
-const highAchievers = '';
+// 2. 평균 점수가 85점 이상인 학생 필터링(filter)
+const highAchievers = studentsWithAverage.filter(function (score) {
+    return score >= 85;
+});
 console.log("우수 학생들:", highAchievers);
 
 
-// 전체 학생들의 평균 점수 계산 (reduce)
+// 3. 전체 학생들의 평균 점수 계산 (reduce)
 const totalAverageScore = '';
 console.log("전체 평균 점수:", totalAverageScore);
 
 
-// 최고 평균 점수를 받은 학생 찾기 (reduce)
+// 4. 최고 평균 점수를 받은 학생 찾기 (reduce)
 const topStudent = '';
 console.log("최고 평균 점수를 받은 학생:", topStudent);
 
 
-// 수학에서 최고 점수를 받은 학생 찾기 (reduce)
+// 5. 수학에서 최고 점수를 받은 학생 찾기 (reduce)
 const topMathStudent = '';
 console.log("수학에서 최고 점수를 받은 학생:", topMathStudent);
 
 
-// 모든 학생들의 점수를 과목별로 합치기 (reduce)
+// 6. 모든 학생들의 점수를 과목별로 합치기 (reduce)
 const subjectScores = '';
 console.log("과목별 점수 합계:", subjectScores);
 
 
-// 과목별 평균 점수 계산하기 (map)
+// 7. 과목별 평균 점수 계산하기 (map)
 const subjectAverages = '';
 console.log("과목별 평균 점수:", subjectAverages);
 
